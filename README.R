2bbd4b44857dc3b381950e72b745518dc0707788
jmontilla <jmontilla@unal.edu.co>
2025-05-07 18:10:36 -0500

    Update README.R
    Signed-off-by: jmontilla <jmontilla@unal.edu.co>

--- README.R
+++ README.R
@@ -1,11 +1,17 @@
-# Hermetia Illucens Project
-
-## Description project analysis and documentation.
-## Structure
-- R/: Source code
-- data-raw/: Raw data
-- data/: Processed data
-- doc/: Documentation
-- output/: Results
-- reports/: Information
-- tests/: Unit tests
+# Create README.md base
+writeLines(
+  c(
+    "# Hermetia Illucens Project",
+    "",
+    "## Description project analysis and documentation.",
+    "## Structure",
+    "- R/: Source code",
+    "- data-raw/: Raw data",
+    "- data/: Processed data",
+    "- doc/: Documentation",
+    "- output/: Results",
+    "- reports/: Information",
+    "- tests/: Unit tests"
+  ),
+  "README.md"
+)
